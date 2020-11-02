c4e1e5ae1a63437295b0f61d12b285f85f21b7b3
ccastleberry <castle.caleb@gmail.com>
2020-11-01 21:18:15 -0500

    added chapter 5 and 6 of rust book

--- The Rust Book/Chapter 2/guessing_game/src/main.rs
+++ The Rust Book/Chapter 2/guessing_game/src/main.rs
@@ -1,14 +1,39 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    let mut guess = String::new();
+    //println!("The secret number is {}", secret_number);
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
     
+    loop{
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
         println!("you guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Equal => {
+                println!("You Win!");
+                break;
+            }
+            Ordering::Greater => println!("Too big!"),
+    }
+    }
+    
 }
