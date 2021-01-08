e6a86f031c02cd0d8c1f76c4d564d197430c4389
ccastleberry <castle.caleb@gmail.com>
2021-01-07 20:43:12 -0500

    checking in before starting again

--- The Rust Book/Chapter 6/enums/src/main.rs
+++ The Rust Book/Chapter 6/enums/src/main.rs
@@ -1,3 +1,24 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
 fn main() {
-    println!("Hello, world!");
+    let coin = Coin::Quarter(UsState::Alabama);
+    let mut count = 0;
+    if let Coin::Quarter(state) = coin {
+        println!("State quarter from {:?}!", state);
+    } else {
+        count += 1;
+    }
+    println!("{}", count);
 }
